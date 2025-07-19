9a0271f9bd357581e9b670582d02f243a7ed41a4
evertonrafaelumbelino <evertonrafaelumbelino@gmail.com>
2025-07-19 20:41:37 -0300

    Adicionando arquivos

---
+++ assets/js/index.js
@@ -0,0 +1,75 @@
+let imgMenu = document.getElementById("imgMenu");
+let menu = document.getElementById("menu");
+let divDepoimentos = document.getElementById("divDepoimentos");
+let blocosDepoimentos = document.getElementsByClassName("depoimentos");
+let depoimento1 = document.getElementById("depoimento1");
+let depoimento2 = document.getElementById("depoimento2");
+let depoimento3 = document.getElementById("depoimento3");
+let depoimento4 = document.getElementById("depoimento4");
+let imgSetaEsquerda = document.getElementById("imgSetaEsquerda");
+let imgSetaDireita = document.getElementById("imgSetaDireita");
+
+imgMenu.addEventListener("click", () => {
+    if (menu.className == "close") {
+        menu.style.position = "fixed";
+        menu.style.top = "2px";
+        menu.style.left = "50%";
+        menu.style.transform = "translate(-50%, 50%)";
+        menu.style.display = "flex";
+        menu.style.flexFlow = "column nowrap";
+        menu.style.textAlign = "center";
+        menu.style.width = "50%";
+        menu.className = "open";
+    } else {
+        menu.style.display = "none";
+        menu.className = "close";
+    }
+});
+
+imgSetaDireita.addEventListener("click", () => {
+    if (depoimento1.style.display == "flex") {
+        depoimento1.style.display = "none";
+        depoimento2.style.display = "flex";
+        depoimento3.style.display = "none";
+        depoimento4.style.display = "none";
+    } else if (depoimento2.style.display == "flex") {
+        depoimento1.style.display = "none";
+        depoimento2.style.display = "none";
+        depoimento3.style.display = "flex";
+        depoimento4.style.display = "none";
+    } else if (depoimento3.style.display == "flex") {
+        depoimento1.style.display = "none";
+        depoimento2.style.display = "none";
+        depoimento3.style.display = "none";
+        depoimento4.style.display = "flex";
+    } else {
+        depoimento1.style.display = "flex";
+        depoimento2.style.display = "none";
+        depoimento3.style.display = "none";
+        depoimento4.style.display = "none";
+    }
+});
+
+imgSetaEsquerda.addEventListener("click", () => {
+    if (depoimento1.style.display == "flex") {
+        depoimento1.style.display = "none";
+        depoimento2.style.display = "none";
+        depoimento3.style.display = "none";
+        depoimento4.style.display = "flex";
+    } else if (depoimento2.style.display == "flex") {
+        depoimento1.style.display = "flex";
+        depoimento2.style.display = "none";
+        depoimento3.style.display = "none";
+        depoimento4.style.display = "none";
+    } else if (depoimento3.style.display == "flex") {
+        depoimento1.style.display = "none";
+        depoimento2.style.display = "flex";
+        depoimento3.style.display = "none";
+        depoimento4.style.display = "none";
+    } else {
+        depoimento1.style.display = "none";
+        depoimento2.style.display = "none";
+        depoimento3.style.display = "flex";
+        depoimento4.style.display = "none";
+    }
+});
